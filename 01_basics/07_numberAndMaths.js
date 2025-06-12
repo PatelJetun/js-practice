71d807928560ad1a2b096d1020264a3229b96e27
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-12 19:02:36 +0000

    Number and Maths

---
+++ 01_basics/07_numberAndMaths.js
@@ -0,0 +1,33 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//**********************MATHS************************ 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.2778321))
+// console.log(Math.ceil(5.2778321))
+// console.log(Math.floor(5.2778321))
+// console.log(Math.min(4,6,5,3,2))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
