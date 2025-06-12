1c68ce937ac4108c1a6b8e6d40ebf4bf35e734a4
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-12 14:55:07 +0000

    Strings in js

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,9 @@
+const name = "jetun"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); //Not modern syntax
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //Backticks are used for template literals which allows string interpolation
+
+const gameName = new String("Call of Duty")
+
