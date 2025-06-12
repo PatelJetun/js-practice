ad8701b9320e068041a6b427b1e4ef9f35c8b241
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-12 19:25:35 +0000

    Date and Time in js

---
+++ 01_basics/08_dateAndTime.js
@@ -0,0 +1,32 @@
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2004, 6, 4)  
+// let myCreatedDate = new Date(2004, 6, 4, 5, 35, 12)  
+let myCreatedDate = new Date("07-04-2004")  
+// console.log(myCreatedDate.toString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
