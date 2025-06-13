cc65beb7144740d4b70d75454202adfddd04bf78
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-13 12:17:56 +0000

    Array in Javascript

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+//array 
+
+const myArray = [0, 1, 2 ,3 ,4, 5]
+const myHeros = ["ironman", "black widow", "reyna"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5, 6, 7)
+// console.log(myArray[0]);
+
+//array methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(4));
+// console.log(myArray.indexOf(2));
+
+// const newArr= myArray.join()
+
+
+// console.log(myArray);
+// console.log(newArr);
+
+//slice, splice
+
+console.log("A ",myArray);
+
+const myn1 = myArray.slice(1, 3)
+
+console.log(myn1);
+console.log("B ",myArray);
+
+const myn2 = myArray.splice(1, 3)
+console.log("C ",myArray);
+console.log(myn2);
