b3f1ccffb8cb9d8ab538a6324812d83830d3acf3
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-15 13:52:31 +0000

    Arrow Functions in JS

---
+++ 03_basics/03_arrowFunctions.js
@@ -0,0 +1,47 @@
+const user = {
+    username: "jetun",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "jetun"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+const chai = () => {
+    let username = "jetun"
+    console.log(this.username)
+}
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) =>( {username: "jetun"} )
+
+
+// console.log(addTwo(3, 4));
+
+const myArr = [2, 4, 6, 7, 9, 10]
