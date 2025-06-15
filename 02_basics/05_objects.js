80f7dbd7279ae688585d506c3fa8f36023cb30ea
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-15 07:40:41 +0000

    JSON structure and object de-structure

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,27 @@
+//destructuring objects
+
+const course = {
+    courseName: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "jetun",
+//     "cousrName": "js hindi",
+//     "price": "free",
+// }
+
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
