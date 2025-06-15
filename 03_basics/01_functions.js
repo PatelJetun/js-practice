752d968568130e57f728d38317e138c047426a12
PatelJetun <165818712+PatelJetun@users.noreply.github.com>
2025-06-15 08:19:46 +0000

    Function with Object in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,34 @@ function loginUserMessage(userName = "sam"){
     return `${userName} just logged in`
 }
 // console.log(loginUserMessage("Jetun"))
-console.log(loginUserMessage("jetun"))
+// console.log(loginUserMessage("jetun"))
+
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 300, 10000, 504));
+
+
+const user = {
+    username: "jetun",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is: ${anyObject.username} and price is: ${anyObject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({username: "Sam", price:399})
+
+const myArr = [200, 400 ,500, 1000]
+
+function returnSecondValue(anyArr){
+    return anyArr[1]
+}
+
+// console.log(returnSecondValue(myArr));
+console.log(returnSecondValue([1000, 6000, 10000, 1000000]));
