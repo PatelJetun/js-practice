2b1c610eebce5a37818905d68b2e45b2396f0f33
PatelJetun <jetunpatel54@gmail.com>
2025-06-24 01:00:13 +0530

    Test

--- 10_objectOrientedProgramming/myClasses.js
+++ 10_objectOrientedProgramming/myClasses.js
@@ -1,39 +1,37 @@
-class User{
-    constructor(username, email, password){
-        this.username = username
-        this.email = email
-        this.password = password
-    }
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
 
-    encryptPassword(){
-        return `qweryuipalms${this.password}abc`
-    }
+  encryptPassword() {
+    return `qweryuipalms${this.password}abc`;
+  }
 
-    changeUserName(){
-        return `${this.username.toUpperCase()}`
-    }
+  changeUserName() {
+    return `${this.username.toUpperCase()}`;
+  }
 }
 
-
-const chai = new User("Jetun", "jetun@google.com", 1234)
+const chai = new User("Jetun", "jetun@google.com", 1234);
 console.log(chai.encryptPassword());
 console.log(chai.changeUserName());
 
-
 //behind the scene
 
-function User(username, email, password){
-    this.username = username
-    this.email = email
-    this.password = password
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
 }
 
-User.prototype.encryptPassword = function(){
-    return `qweryuipalms${this.password}abc`;
-}
+User.prototype.encryptPassword = function () {
+  return `qweryuipalms${this.password}abc`;
+};
 
 User.prototype.changeUserName = function () {
-    return `${this.username.toUpperCase()}`;    
+  return `${this.username.toUpperCase()}`;
 };
 
 const tea = new User("tea", "tead@google.com", 1245);
