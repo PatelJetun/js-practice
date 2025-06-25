a0dc657214831d5a1c694fab5a2d8ebab8820808
PatelJetun <jetunpatel54@gmail.com>
2025-06-25 14:38:04 +0530

    Time Counter is displayed for each card and its interval is cleared when card is deleted

--- Personal Task Manager/index.js
+++ Personal Task Manager/index.js
@@ -39,41 +39,54 @@ function checkInput() {
 }
 
 function addTaskCard() {
-  //Adds task Card
-  taskCardContainer.innerHTML += `<div class="task-card">
-            <p clas="task-name">Task: <span>${taskName.value}</span> </p>
-            <p clas="task-end">End: <span>${endTime.value}</span> </p>
-            <p clas="task-end">Priority Level: <span>${priorityLevel.value}</span> </p>
-            <button class="reminder-btn"><i class="fa-solid fa-bell"></i>Reminder</button>
-            <i class="fa-solid fa-pen edit-btn"></i> 
-            <i class="fa-solid fa-trash delete-btn"></i>
-            <p class="time-counter">Time: <span></span></p>
-    </div>`;
-}
+  //Adds task Car
+  const taskCard = document.createElement("div");
+  taskCard.classList.add("task-card");
 
+  taskCard.innerHTML = `<p clas="task-name">Task: <span>${taskName.value}</span> </p>
+  <p class="task-end">End: <span>${endTime.value}</span> </p>
+  <p class="task-end">Priority Level: <span>${priorityLevel.value}</span> </p>
+  <button class="reminder-btn"><i class="fa-solid fa-bell"></i>Reminder</button>
+  <i class="fa-solid fa-pen edit-btn"></i> 
+  <i class="fa-solid fa-trash delete-btn"></i>
+  <p class="time-counter">Time: <span>Test Time</span></p>`;
+
+  taskCardContainer.appendChild(taskCard);
+
+  const timeSpan = taskCard.querySelector(".time-counter span")
+  calculateTime(endTime.value, timeSpan)
+  const timerId =   calculateTime(endTime.value, timeSpan);
+  taskCard.timerId = timerId; 
+}
 function removeCard(card) {
   //Remove Card
+  if (card.timerId){
+    console.log(`Clear Interval Called ${card.timerId} freed`);
+    clearInterval(card.timerID)
+  }
   card.remove();
 }
 
-function calculateTime(end) {
+function calculateTime(end, displayElement) {
   const endTime = new Date(end);
 
   function updateCounter() {
-    const currentTime = new Date()
+    const currentTime = new Date();
     const remaining = endTime - currentTime;
 
-    if (remaining < 0) {
+    if (remaining <= 0) {
       clearInterval(timer);
-      return "Time Up!";
+      displayElement.textContent = "Time is Up!"
+      return;
     }
 
     const hours = Math.floor(remaining / (1000 * 60 * 60));
     const minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
 
-    return `${hours}h ${minutes}m ${seconds}s`
+    displayElement.textContent = `${hours}h ${minutes}m ${seconds}s`;
   }
-  updateCounter();
+  updateCounter(); 
   const timer = setInterval(updateCounter, 1000);
+  return timer;
 }
