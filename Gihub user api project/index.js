bc091970aa53d863cd828b9fd2c4f39a7d48c132
PatelJetun <jetunpatel54@gmail.com>
2025-06-29 11:26:37 +0530

    Search Box Shows User Name when searched for

--- Gihub user api project/index.js
+++ Gihub user api project/index.js
@@ -1,8 +1,5 @@
 const searchBar = document.querySelector("#search-bar");
 const searchBox = document.querySelector("#search-box");
-const searchBoxData = document.querySelectorAll('#search-box li')
-
-console.log(searchBoxData);
 
 // searchBox.innerHTML = ''
 
@@ -16,28 +13,42 @@ searchBar.addEventListener("blur", () => {
   hideSearchBox();
 });
 
-searchBar.addEventListener('keyup', () => {
+const dgetData = debounce((searchQuery) => {
+  searchBox.innerHTML = `
+    <div id="search-spinner" style="display:block; text-align:center; padding:1rem;">
+      <div class="spinner"></div>
+    </div>
+  `;
+  showSpinner();
+  getSearchData(searchQuery).then((data) => {
+    hideSpinner();
+    if (data && data.items) {
+      fillSearchBox(data.items);
+    } else {
+      searchBox.innerHTML = "<li>No results found</li>";
+    }
+  });
+}, 300);
 
-    const filter = searchBar.value.toUpperCase()
+searchBar.addEventListener("keyup", () => {
+  const searchQuery = searchBar.value.toUpperCase();
+  dgetData(searchQuery);
+  // searchBoxData.forEach(values => {
+  //     // console.log(values);
 
-    searchBoxData.forEach(values => {
-        // console.log(values);
-        if(values.innerText.toUpperCase().indexOf(filter) > -1){
-            values.style.display= '';
-        } else {
-            values.style.display= 'none';
-        }
-    })
+  //     if(values.innerText.toUpperCase().indexOf(filter) > -1){
+  //         values.style.display= '';
+  //     } else {
+  //         values.style.display= 'none';
+  //     }
+  // })
 });
-
 
 searchBox.addEventListener("mousedown", (e) => {
   if (e.target.classList.contains("search-box-list")) {
     console.log(`Value: ${e.target.innerText}`);
   }
 });
-
-
 
 function displaySearchBox() {
   // console.log("displaySeach called");
@@ -47,3 +58,50 @@ function displaySearchBox() {
 function hideSearchBox() {
   searchBox.style.display = "none";
 }
+
+function showSpinner() {
+  document.getElementById("search-spinner").style.display = "block";
+}
+function hideSpinner() {
+  document.getElementById("search-spinner").style.display = "none";
+}
+
+function fillSearchBox(users) {
+  searchBox.innerHTML = `
+  <div id="search-spinner" style="display:none; text-align:center; padding:1rem;">
+    <div class="spinner"></div>
+  </div>
+`;
+  users.forEach((user) => {
+    const li = document.createElement(`li`);
+    li.classList.add("search-box-list");
+    li.innerText = user.login;
+    searchBox.appendChild(li);
+  });
+}
+
+function debounce(func, delay) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+async function getSearchData(search) {
+  const url = `https://api.github.com/search/users?q=${search}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// https://api.github.com/search/users?q=
